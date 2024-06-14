c141adc7b3007556fa069ff390beca7e48191ad2
rafald1 <116283372+rafald1@users.noreply.github.com>
2024-06-14 07:03:24 +0200

    Update part_1.rs
    Removing assert_eq! that was used when refactoring the code.

--- src/day_23_amphipod/part_1.rs
+++ src/day_23_amphipod/part_1.rs
@@ -124,7 +124,6 @@ fn process_data(path: &str) -> [usize; 19] {
 pub fn solve() -> String {
     let burrow_state = process_data("./input/23.txt");
     let result = calculate_minimum_energy_cost(burrow_state);
-    assert_eq!(result, 14348);
     format!("Day 23: Amphipod (Part 1) answer: {}", result)
 }
 
